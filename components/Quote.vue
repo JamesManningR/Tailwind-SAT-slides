31e1f07019f0749feaf99174d9952e7c0cc2c02b
JamesManningR <jamesmanningr98@gmail.com>
2023-02-03 17:14:41 +0000

    created first 2 slides

--- components/Quote.vue
+++ components/Quote.vue
@@ -1,10 +1,10 @@
 <template>
-  <figcaption class="mt-6 flex items-center justify-center space-x-4 text-left"><img
-      src="https://tailwindcss.com/_next/static/media/adam.26d0119c.jpg" alt="" class="w-14 h-14 rounded-full"
-      loading="lazy" decoding="async">
+  <figcaption class="flex items-center space-x-4 text-right">
     <div>
       <div class="text-slate-900 font-semibold dark:text-white">Adam Wathan</div>
       <div class="mt-0.5 text-sm leading-6">Creator of Tailwind CSS</div>
     </div>
+    <img src="https://tailwindcss.com/_next/static/media/adam.26d0119c.jpg" alt="" class="w-14 h-14 rounded-full"
+      loading="lazy" decoding="async">
   </figcaption>
 </template>
